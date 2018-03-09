ad0dc085e3d81c358e88d162554ea6fabdf95255
yash0307 <yash.patel@students.iiit.ac.in>
2018-03-08 21:06:33 -0500

    + sync

--- DQN_Implementation.py
+++ DQN_Implementation.py
@@ -30,10 +30,6 @@ class QNetwork():
           self.model_1 = self.LinearDQN_initialize(model_type)
           self.save_model_weights(self.model_1, 'model_init_'+environment_name+'.h5')
           self.model_2 = load_model('model_init_'+environment_name+'.h5')
-        elif environment_name == 'MountainCar-v0':
-          self.model_1 = self.LinearDQN_initialize(model_type)
-          self.save_model_weights(self.model_1, 'model_init_'+environment_name+'.h5')
-          self.model_2 = load_model('model_init_'+environment_name+'.h5') 
         else:
           self.model = self.LinearDQN_initialize(model_type)
         del env
@@ -61,16 +57,16 @@ class QNetwork():
 
         if model_type == 'dqn':
             model = Sequential()
-            model.add(Dense(16, input_dim=self.state_size, activation='relu'))
-            model.add(Dense(16, input_dim=self.state_size, activation='relu'))
+            model.add(Dense(32, input_dim=self.state_size, activation='relu'))
+            model.add(Dense(32, input_dim=self.state_size, activation='relu'))
             model.add(Dense(self.num_actions, activation='linear'))
 
         if model_type == 'ddqn':
             input_layer = Input(shape=(self.state_size,))
-            x = Dense(16, activation='relu')(input_layer)
+            x = Dense(32, activation='relu')(input_layer)
             state_val = Dense(1, activation='linear')(x)
-            y = Dense(16, activation='relu')(input_layer)
-            y = Dense(16, activation='relu')(y)
+            y = Dense(32, activation='relu')(input_layer)
+            y = Dense(32, activation='relu')(y)
             adv_vals = Dense(self.num_actions, activation='linear')(y)
             policy = keras.layers.merge([adv_vals, state_val], mode=lambda x: x[0]-K.mean(x[0])+x[1], output_shape = (self.num_actions,))
             model = Model(input=[input_layer], output=[policy])
@@ -159,6 +155,19 @@ class QNetwork():
         else:
             print('UNKNOWN MODEL NUMBER AT TRAIN !')
             sys.exit(1)
+
+    def train_batch_mountain_car(self, states, actions, rewards, next_states, dones, gamma):
+        batch_size = states.shape[0]
+	targets = rewards
+	targets_f = np.zeros((batch_size, self.num_actions), dtype='float')
+	for i in range(0, batch_size):
+	    if not dones[i]:
+	        targets[i] = float(rewards[i] + gamma*np.amax(self.model.predict(np.reshape(next_states[i], [1,self.state_size]))[0]))
+            elif dones[i] and next_states[i][0] >= 0.5:
+	        targets[i] = float(rewards[i])
+	    targets_f[i][:] = self.model.predict(np.reshape(states[i], [1,self.state_size]))[0]
+	    targets_f[i][int(actions[i])] = targets[i]
+	self.model.fit(states, targets_f, epochs=1, verbose=0)
 
     def train_batch_space_invaders(self, states, actions, rewards, next_states, dones, gamma, model_num):
         batch_size = states.shape[0]
@@ -287,12 +296,12 @@ class DQN_Agent():
         self.env.reset()
         if environment_name == 'CartPole-v0':
             self.gamma = float(0.99)
-            self.model_type = 'ddqn'
-            self.train_type = 'no_replay_memory'
+            self.model_type = 'dqn'
+            self.train_type = 'use_replay_memory'
         if environment_name == 'MountainCar-v0':
             self.gamma = float(1)
             self.train_type = 'use_replay_memory'
-            self.model_type = 'ddqn'
+            self.model_type = 'dqn'
         if environment_name == 'SpaceInvaders-v0':
             self.gamma = float(1)
             self.model_type = 'dqn_space_invaders'
@@ -329,6 +338,7 @@ class DQN_Agent():
     def train(self):
 
         if self.train_type == 'no_replay_memory':
+	    test_rewards = np.zeros((int(self.num_episodes/100)+1,1))
             for given_episode in range(0, self.num_episodes):
                 print('Train episode: ' + str(given_episode))
                 state = self.env.reset()
@@ -348,20 +358,25 @@ class DQN_Agent():
                     state = next_state
                     if done:
                         break
+	        if given_episode%100 == 0 and given_episode != 0:
+		    test_rewards[int(given_episode/100)] = self.test_cartpole(test_iters=1)
+            print(test_rewards)
+
         elif self.train_type == 'use_replay_memory' and self.env_name == 'MountainCar-v0':
+	    rand_thresh = 1
+	    test_rewards = np.zeros((int(self.num_episodes/100)+1,1))
             for given_episode in range(0, self.num_episodes):
                 print('Train episode: ' + str(given_episode))
                 state = self.env.reset()
                 state = np.reshape(state, [1 , self.state_size])
-                rand_thresh = 1
+		rand_thresh *= self.eps_decay_fact
+		rand_thresh = max(rand_thresh, 0.1)
                 for given_iter in range(0, self.num_iterations):
-                    rand_thresh *= self.eps_decay_fact
-                    rand_thresh = max(rand_thresh, 0.1)
-                    rand_num = np.random.uniform(low=0, high=1)
+		    rand_num = np.random.uniform(low=0, high=1)
                     if rand_num < rand_thresh:
                         action = np.random.randint(0, self.num_actions, 1)[0]
                     else:
-                        action = self.model.get_action_dual_model(state, model_num = 1)
+                        action = self.model.get_action(state)
                     next_state, reward, done, _ = self.env.step(action)
                     next_state = np.reshape(next_state, [1, self.state_size])
                     self.replay_memory.append([state, action, reward, next_state, done])
@@ -373,12 +388,13 @@ class DQN_Agent():
                     rewards = np.array([i[2] for i in given_batch], dtype='float')
                     next_states = np.array([i[3][0] for i in given_batch], dtype='float')
                     dones = np.array([i[4] for i in given_batch], dtype='bool')
-                    self.model.train_batch_dual_model(states, actions, rewards, next_states, dones, self.gamma, model_num=2)
-                self.model.save_model_weights(self.model.model_2, 'model_init_'+self.env_name+'.h5')
-                self.model.checkpoint_swap(self.env_name)
-                self.test(test_iters=1)
+                    self.model.train_batch_mountain_car(states, actions, rewards, next_states, dones, self.gamma)
+            if given_episode%100 == 0 and given_episode != 0:
+                test_rewards[int(given_episode/100)] = self.test(test_iters=1)
+            print(test_rewards)
 
         elif self.train_type == 'use_replay_memory':
+	    test_rewards = np.zeros((int(self.num_episodes/100)+1,1))
             for given_episode in range(0, self.num_episodes):
                 print('Train episode: ' + str(given_episode))
                 state = self.env.reset()
@@ -404,7 +420,9 @@ class DQN_Agent():
                     next_states = np.array([i[3][0] for i in given_batch], dtype='float')
                     dones = np.array([i[4] for i in given_batch], dtype='bool')
                     self.model.train_batch(states, actions, rewards, next_states, dones, self.gamma)
-                self.test(test_iters=1)
+                if given_episode%100 == 0 and given_episode != 0:
+	            test_rewards[int(given_episode/100)] = self.test_cartpole(test_iters=1)
+	    print(test_rewards)
 
         elif self.train_type == 'use_replay_memory_space_invaders_v0':
             given_batch = self.replay_memory.sample_batch(self.batch_size)
@@ -457,12 +475,29 @@ class DQN_Agent():
             total_reward = 0
             while done is False:
                 state = np.reshape(state, [1, self.state_size])
-                action = self.model.get_action_dual_model(state, model_num=2)
+                action = self.model.get_action(state)
                 next_state, reward, done, _ = self.env.step(action)
                 total_reward += reward
                 state = next_state
             avg_reward += total_reward
         print(float(avg_reward) / float(test_iters))
+	return (float(avg_reward) / float(test_iters))
+
+    def test_cartpole(self, test_iters, model_file=None):
+        avg_reward = 0
+	for given_episode in range(0, test_iters):
+	    state = self.env.reset()
+	    done = False
+	    total_reward = 0
+	    while done is False:
+	        state = np.reshape(state, [1, self.state_size])
+		action = self.model.get_action(state)
+		next_state, reward, done, _ = self.env.step(action)
+		total_reward += reward
+		state = next_state
+            avg_reward += total_reward
+	print(float(avg_reward) / float(test_iters))
+	return(float(avg_reward) / float(test_iters))
 
     def test_image(self, test_iters, model_file=None):
         avg_reward = 0
@@ -514,7 +549,7 @@ def main(args):
     environment_name = args.env
     agent = DQN_Agent(environment_name)
     agent.train()
-    agent.test_image(test_iters=200)
+    agent.test_cartpole(test_iters=200)
 
 
 if __name__ == '__main__':
